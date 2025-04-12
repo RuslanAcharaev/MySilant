15fe77d2bd42bd708154f22c89c75c0a65f0090a
Ruslan Acharaev <racharaev@gmail.com>
2025-04-13 01:13:25 +0300

    Edited CORS_ALLOWED_ORIGINS #2

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -126,10 +126,15 @@ REST_FRAMEWORK = {
 }
 
 CORS_ALLOWED_ORIGINS = [
+    "http://localhost",
+    "http://127.0.0.1",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "http://localhost:80",
+    "http://127.0.0.1:80",
+    "http://frontend:3000",
 ]
 
 SIMPLE_JWT = {
