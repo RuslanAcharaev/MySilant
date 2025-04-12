6f367a32e9c574f184b60d9ee382ec24bf45838d
Ruslan Acharaev <racharaev@gmail.com>
2025-04-13 02:11:18 +0300

    Edited nginx.conf

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -12,6 +12,6 @@ urlpatterns = [
     path('api/', include('api.urls')),
     path('api/token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', CustomTokenRefresh.as_view(), name='token_refresh'),
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
